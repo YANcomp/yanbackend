305513707bf252f5a8ac3a2e76c387619d4e0237
dnaugolnyi <dnaugolnyi@yandex.ru>
2024-01-21 22:36:19 +0300

    ver 0.1.21.01.2208

--- stories_service/migrations/20230729123132_create_stories_table.sql
+++ stories_service/migrations/20230729123132_create_stories_table.sql
@@ -1,12 +1,30 @@
 -- +goose Up
+BEGIN;
+
+SET
+    statement_timeout = 0;
+SET
+    client_encoding = 'UTF8';
+SET
+    standard_conforming_strings = ON;
+SET
+    check_function_bodies = FALSE;
+SET
+    client_min_messages = WARNING;
+SET
+    search_path = public, extensions;
+SET
+    default_tablespace = '';
+SET
+    default_with_oids = FALSE;
+
+-- EXTENSIONS --
 CREATE
-EXTENSION IF NOT EXISTS citext;
+    EXTENSION IF NOT EXISTS pgcrypto;
 CREATE
-EXTENSION IF NOT EXISTS POSTGIS;
-CREATE
-EXTENSION IF NOT EXISTS pg_trgm;
-CREATE
-EXTENSION IF NOT EXISTS btree_gist;
+    EXTENSION IF NOT EXISTS citext;
+
+-- DROP TABLE --
 DROP TABLE IF EXISTS stories CASCADE;
 DROP TABLE IF EXISTS slides CASCADE;
 
@@ -36,6 +54,8 @@ create table slides
     created_at         TIMESTAMP WITH TIME ZONE DEFAULT now(),
     updated_at         TIMESTAMP WITH TIME ZONE
 );
+
+COMMIT;
 
 -- +goose Down
 drop table stories;
